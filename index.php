749eadd093414d684730b7ef867515edf334f0b4
julianafrench <julianaefrench@gmail.com>
2016-11-28 16:19:04 -0800

    math game now plays

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php session_start(); ?>
+<?php session_start(); 
+    if (!isset($_SESSION["email"])) {
+        header("Location:login.php");
+        die();
+    }
+?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
